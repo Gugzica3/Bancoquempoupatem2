76b42da72707349037707cecd1274e814dfe1309
Gugzica3 <126602869+Gugzica3@users.noreply.github.com>
2023-10-24 13:57:50 -0300

    Create biblioteca.c
    Criação da biblioteca

---
+++ biblioteca.c
@@ -0,0 +1,14 @@
+//
+// Created by gusta on 24/10/2023.
+//
+
+#include "biblioteca.h"
+
+int CompararString(const char *str1, const char *str2) {
+    while (*str1 && *str2 && *str1 == *str2) {
+        str1++;
+        str2++;
+    }
+
+    return *(unsigned char *)str1 - *(unsigned char *)str2;
+}
