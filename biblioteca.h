6e78b0fee81348e1512c219c9aa13db74c34a632
Gugzica3 <126602869+Gugzica3@users.noreply.github.com>
2023-10-24 13:58:18 -0300

    Create biblioteca.h
    Criação da biblioteca

---
+++ biblioteca.h
@@ -0,0 +1,10 @@
+//
+// Created by gusta on 24/10/2023.
+//
+
+#ifndef UNTITLED1_BIBLIOTECA_H
+#define UNTITLED1_BIBLIOTECA_H
+
+int CompararString(const char *str1, const char *str2);
+
+#endif //UNTITLED1_BIBLIOTECA_H
